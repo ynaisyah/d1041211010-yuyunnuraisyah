3e9dcd56d49fbac706750a25623e6d13028b9430
ynaisyah <d1041211010@student.untan.ac.id>
2024-09-08 23:00:04 +0700

    Menambahkan

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ def luas_segitiga():
 
 luas_segitiga()
 
+#Menghitung Luas Persegi Panjang
+def luas_persegi_panjang():
+    p = int(input("Masukan panjang persegi panjang:"))
+    l = int(input("Masukan lebar persegi panjang:"))
+    luas = p * l
+    print("Luas Persegi Panjang adalah: ", luas)
+
+luas_persegi_panjang()
+
